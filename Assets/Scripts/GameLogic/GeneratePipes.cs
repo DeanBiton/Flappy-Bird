7a26af35b6d375326e6f07a20f27384aeb1a4faa
Dean <dinbiton100@gmail.com>
2022-08-16 14:57:06 +0300

    foreground animation

--- Assets/Scripts/GameLogic/GeneratePipes.cs
+++ Assets/Scripts/GameLogic/GeneratePipes.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class GeneratePipes : MonoBehaviour
 {
-    private bool end = false;
+    private bool endGame = false;
     [SerializeField] private float cooldown = 2;
+    [SerializeField] private float range = 32f;
     [SerializeField] private GameObject prefabPipe;
     private Vector3 pipeStartPosition;
     void Start()
@@ -21,10 +22,10 @@ public class GeneratePipes : MonoBehaviour
 
     IEnumerator generatePipes()
     {
-        while(!end)
+        while(!endGame)
         {
             yield return new WaitForSeconds(cooldown);
-            Instantiate(prefabPipe, pipeStartPosition + new Vector3(0,Random.Range(0f,32f),0), Quaternion.identity);
+            Instantiate(prefabPipe, pipeStartPosition + new Vector3(0,Random.Range(0f,range),0), Quaternion.identity);
         }
     }
 }
